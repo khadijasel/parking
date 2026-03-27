1ccfa32b8d0d87dc922bea2f7a5f0c7847d06983
itsafaf <mynameafaf20@gmail.com>
2026-03-27 16:07:06 +0100

    Update build configuration and dependencies

--- parking_front/lib/main.dart
+++ parking_front/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
-import 'features/parking/presentation/map_home_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:parking_front/features/profile/presentation/screens/my_reservations_screen.dart';
+import 'package:parking_front/features/reservation/presentation/screens/reservation_screen.dart';
+import 'package:parking_front/features/scanner/presentation/screens/scanner_screen.dart';
+import 'features/main/main_screen.dart';
 import 'theme/app_theme.dart';
+import 'features/splash/presentation/screens/splash_screen.dart';
+import 'features/parking/presentation/map_home_screen.dart';
+import 'features/payment/presentation/screens/payment_screen.dart';
 
 void main() {
-  runApp(const SmartParkApp());
+  runApp(const ProviderScope(child: SmartParkApp()));
 }
 
 class SmartParkApp extends StatelessWidget {
@@ -15,7 +22,17 @@ class SmartParkApp extends StatelessWidget {
       title: 'smartpark',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const MapHomeScreen(),
+      home: const SplashScreen(nextRoute: MainScreen()),
+      //home: const SplashScreen(nextRoute: MapHomeScreen()),
+      //home: const HomeScreen(),
+     /* home: const PaymentScreen(
+        sessionId: 'test_session',
+        userId: 'test_user',
+        parkingName: 'Parking Sidi Yahia',
+        dureeMinutes: 90,
+      ),*/
+      //home: const ReservationScreen(),
+      
     );
   }
 }
